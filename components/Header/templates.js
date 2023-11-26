91be6974c8b7056e12382e08ad8eaaa1ea2eafe9
jeeseongmin <peration0422@exntu.com>
2023-11-26 13:50:30 +0900

    feat : Header 컴포넌트 구성

---
+++ components/Header/templates.js
@@ -0,0 +1,9 @@
+export const HeaderTemplates = ({ title }) => {
+  return `
+      <div></div>
+      <h1>${title}</h1>
+      <span id="modal-open-button" class="icon material-symbols-outlined">
+        leaderboard
+      </span>
+  `;
+};
